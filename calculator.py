351bfdcf742db5ac432935e2e71cd46618ea01ee
Machine Learning Engineer <ankushmulkar.it@gmail.com>
2023-04-16 10:52:11 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -43,13 +43,40 @@ def main():
     num2 = st.number_input("Enter second number:")
     operation = st.selectbox("Select operation:", ["+", "-", "*", "/", "sin", "cos", "tan"])
     
+    # Create digit buttons
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        if st.button("1"):
+            num1 = num1*10 + 1
+        if st.button("4"):
+            num1 = num1*10 + 4
+        if st.button("7"):
+            num1 = num1*10 + 7
+    with col2:
+        if st.button("2"):
+            num1 = num1*10 + 2
+        if st.button("5"):
+            num1 = num1*10 + 5
+        if st.button("8"):
+            num1 = num1*10 + 8
+        if st.button("0"):
+            num1 = num1*10 + 0
+    with col3:
+        if st.button("3"):
+            num1 = num1*10 + 3
+        if st.button("6"):
+            num1 = num1*10 + 6
+        if st.button("9"):
+            num1 = num1*10 + 9
+    
     # Perform calculation and display result
-    if operation in ["+", "-", "*", "/"]:
-        result = calculate(num1, num2, operation)
-        st.write("Result:", format_result(result))
-    elif operation in ["sin", "cos", "tan"]:
-        result = calculate_trig(num1, operation)
-        st.write("Result:", format_result(result))
+    if st.button("Calculate"):
+        if operation in ["+", "-", "*", "/"]:
+            result = calculate(num1, num2, operation)
+            st.write("Result:", format_result(result))
+        elif operation in ["sin", "cos", "tan"]:
+            result = calculate_trig(num1, operation)
+            st.write("Result:", format_result(result))
 
 # Run the app
 if __name__ == "__main__":
